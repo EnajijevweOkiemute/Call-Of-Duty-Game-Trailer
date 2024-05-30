1a7fc41ca3704a71279c90631b98c53edf312692
Evaloved <38355451+EnajijevweOkiemute@users.noreply.github.com>
2024-05-30 05:50:50 +0100

    Add files via upload

---
+++ CallOfDutyGame/script.js
@@ -0,0 +1,7 @@
+
+
+const background = document.querySelectorAll('.cod')[2];
+background.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url('images/call_of_duty.jpg')";
+    document.body.style.opacity = '0';
+}); 
